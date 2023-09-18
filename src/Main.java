446d17754abfaf7503561d7057141270eaef4a7c
Snapter88 <jani.jokinen88@gmail.com>
2023-09-18 21:29:55 +0300

    Lisätty metodi
    Lisätty metodi, korjattu kirjoitusvirheitä , kommentoitu ja lisätty String tyyppisiin scannereihin toLowercase. Korjattu myös voittoehto lauseeseen += oikein

--- src/Main.java
+++ src/Main.java
@@ -9,15 +9,13 @@ public class Main {
 		
 		Scanner scanner = new Scanner(System.in);
 		int randomNumber[] = new int[3];
-		String continueGame = "e";
+		String continueGame = "e".toLowerCase();
 		int playerMoney = 0;
 		
 		System.out.println("Tervetuloa Lucky7 kasinolle!");
-		System.out.println("Kone arpoo sinulle 3 numeroa, välillä 1-10 ja 7 voittaa");
+		System.out.println("Kone arpoo sinulle 3 numeroa välillä 1-10 ja 7 voittaa");
 		
-		System.out.println("Paljonko rahaa haluat laittaa? ");
-		playerMoney = scanner.nextInt();
-			
+		playerMoney = welcomeLoop();
 		System.out.println("Haluatko jatkaa peliin (k/e)? ");
 		continueGame = scanner.next();
 			
@@ -30,24 +28,25 @@ public class Main {
 				System.out.println("Numerot ovat " + Arrays.toString(randomNumber));	
 
 				playerMoney--;
+
 				
-				// if else lause joka määrittää voittoehdot
+				// if-else lause joka määrittää voittoehdot
 				if(randomNumber[0] == 7 && randomNumber[1] == 7 && randomNumber[2] == 7) {
 					System.out.println("Voitit 8€");
-					playerMoney =+ 8;
+					playerMoney += 8;
 				} else if(randomNumber[0] == 7 && randomNumber[1] == 7 || randomNumber[0] == 7 && randomNumber[2] == 7) {
 					System.out.println("Voitit 4€");
-					playerMoney =+ 4;
+					playerMoney += 4;
 				} else if (randomNumber[0] == 7 || randomNumber[1] == 7 || randomNumber[2] == 7) {
 					System.out.println("Voitit 2€");
-					playerMoney =+ 2;
+					playerMoney += 2;
 				} else {
 					System.out.println("Ei voittoa");
 				}
-				
+
 				// if lause joka kertoo mitä tehdään kun pelaajalta loppuvat rahat
 				if(playerMoney == 0) {
-					System.out.println("Rahat loppuivat. Haluatko lisätä rahaa?");
+					System.out.println("Rahat loppuivat. Haluatko lisätä rahaa (k/e)?");
 					continueGame = scanner.next();
 					if(continueGame.equals("k")) {
 						System.out.println("Paljonko lisätään?");
@@ -56,9 +55,17 @@ public class Main {
 						System.out.println("Kiitos että pelasit Lucky7 kasinolla");
 						break;
 					}
+				} 
+				
+				System.out.println("Sinulla on jäljellä " + playerMoney + "€");
+				System.out.println("Haluatko pelata uudestaan (k/e)?");
+				continueGame = scanner.next();
+				
+				// if lause joka keskeyttää pelin jos pelaaja syöttää e
+				if(continueGame.equals("e")) {
+					System.out.println("Kiitos että pelasit Lucky7 kasinolla");
+					break;
 				}
-				System.out.println("Haluatko pelata uudestaan?");
-				continueGame = scanner.next();		
 			}
 	
 	} // End main
@@ -68,5 +75,18 @@ public class Main {
 		int randomGenerate = random.nextInt(10) + 1;
 		return randomGenerate;
 	
+	}
+	public static int welcomeLoop() {
+		Scanner scanner = new Scanner(System.in);
+		int playerMoney = 0;
+		while(playerMoney == 0) {
+			System.out.println("Paljonko rahaa haluat laittaa? ");
+			playerMoney = scanner.nextInt();
+			if(playerMoney == 0) {
+				System.out.println("Arvon pitää olla yli 0");
+				continue;
+			}
+		}
+		return playerMoney;
 	}
 }
